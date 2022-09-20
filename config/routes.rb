75b0305fd7a4cd809ec95d21a07c92e425b7c004
dainiuxt <dainiuxt@gmail.com>
2022-09-20 12:07:52 +0300

    add static template and routes for examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "projects#index"
+  get 'sample', to: "static_pages#sample"
   
   # resources :items
-
+  
   resources :projects do
     resources :items
   end
+
 end
