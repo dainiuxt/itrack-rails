efd1527fa34fa1f0658355ce1579975a409c83f4
dainiuxt <dainiuxt@gmail.com>
2022-10-09 20:37:40 +0300

    refactor forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    # @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def new
