5ec70ad1bfa316e97efedceb5fddb518a35c663f
dainiuxt <dainiuxt@gmail.com>
2022-10-13 17:19:12 +0300

    add pagination and style to items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def page_entries_info(collection, options = {})
+    entry_name = options[:entry_name] || (collection.empty?? 'item' :
+        collection.first.class.name.split('::').last.titleize)
+    if collection.total_pages < 2
+      case collection.size
+      when 0; "No #{entry_name.pluralize} found"
+      else; "Displaying all #{entry_name.pluralize}"
+      end
+    else
+      %{#{entry_name.pluralize} %d - %d of %d} % [
+        collection.offset + 1,
+        collection.offset + collection.length,
+        collection.total_entries
+      ]
+    end
+  end
 end
