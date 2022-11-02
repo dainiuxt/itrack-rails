ecac0c18cd108fa97e368115c8d0a69491298cb6
dainiuxt <dainiuxt@gmail.com>
2022-11-02 15:58:09 +0200

    add pundit gem and user to role association

--- Gemfile
+++ Gemfile
@@ -80,3 +80,5 @@ end
 gem "tailwindcss-rails", "~> 2.0"
 
 gem "devise", "~> 4.8"
+
+gem "pundit", "~> 2.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_131157) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_134802) do
   create_table "issues", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_131157) do
     t.integer "assignrd_to"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -58,5 +64,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_131157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_roles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "role_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
   add_foreign_key "issues", "projects"
+  add_foreign_key "users_roles", "roles"
+  add_foreign_key "users_roles", "users"
 end
